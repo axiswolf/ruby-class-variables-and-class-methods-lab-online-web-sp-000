5781580755f4e2efd322cf265fa844f8f0fc6a43
Karen V <karenvo@usc.edu>
2019-03-20 09:01:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,24 +11,24 @@ class Song
     @@artists << artist
     @@genres << genre
   end
-  def self.count 
+  def self.count
      @@count
    end
- 
+
     def self.artists
      @@artists.uniq
    end
- 
+
     def self.genres
      @@genres.uniq
    end
- 
+
     def self.genre_count
      genre_count = Hash.new(0)
      @@genres.each {|genre| genre_count[genre] += 1}
      genre_count
    end
- 
+
     def self.artist_count
      artist_count = Hash.new(0)
      @@artists.each {|artist| artist_count[artist] += 1}
